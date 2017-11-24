b49a0177dde7a0f99ce8dc15db72a78307ace6d2
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-11-24 14:06:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
     return "HUH?! SPEAK UP, SONNY!"
   elsif "I LOVE YOU GRANDMA!" == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
-  else "I LOVE YOU GRANDMA!"
+  else "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
